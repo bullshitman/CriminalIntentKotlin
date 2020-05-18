075ee194b777327ec8932243bbf95dbe4ad5093d
Bullshitman <sokol.joe@gmail.com>
2020-05-18 13:48:08 +0300

    Chapter8

--- app/src/main/java/com/bullshitman/criminalintent/MainActivity.kt
+++ app/src/main/java/com/bullshitman/criminalintent/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val currentFragmentManager = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragmentManager == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit()
+        }
     }
 }
